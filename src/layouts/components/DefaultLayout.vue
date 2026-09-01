6207897a8dfb94210bcc9300b1d1bf3348d20c53
Thomas <124535458+Thomas-Cabrit@users.noreply.github.com>
2026-09-01 21:07:30 +0200

    feat(branding): make Xitcoin identity primary

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -92,8 +92,8 @@ const show_ad = computed(() => {
     >
       <div class="flex justify-between mt-1 pl-4 py-4 mb-1">
         <RouterLink to="/" class="flex items-center">
-          <img class="w-10 h-10" src="../../assets/logo.svg" />
-          <h1 class="flex-1 ml-3 text-2xl font-semibold dark:text-white">Ping.pub</h1>
+          <img class="h-10 w-10 rounded-md" src="/assets/xitcoin-logo.png" alt="Xitcoin" />
+          <h1 class="ml-3 flex-1 text-2xl font-semibold dark:text-white">Xitcoin Explorer</h1>
         </RouterLink>
         <div
           class="pr-4 cursor-pointer xl:!hidden"
